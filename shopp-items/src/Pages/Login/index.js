75f060efe605a1e9952e5a60362f837b93701b1f
joaovitorfreitas <joaovdlf@gmail.com>
2021-09-20 16:04:21 -0300

    mobile

--- shopp-items/src/Pages/Login/index.js
+++ shopp-items/src/Pages/Login/index.js
@@ -53,57 +53,56 @@ export const Login = () =>{
         <>
             <div className="BackGroundLogin">
                 <div className="BorderBoxLogin">
-                <Row
-                    className="RemoveSpacingCad"
-                >
-                    <Col>
-                        <img
-                            className="FundoLogin"
-                            src={BannerLogin}
-                        />
-                    </Col>
-                    <Col className="EmailpwdSide">
-                    <div style={{marginTop: 20}}>
-                        <h1 style={{color: "#D95843"}}>Login</h1>
-                    </div>
-
-                    <Form
-                        className="FormLoginWidht"
+                    <Row
+                        className="RemoveSpacingCad"
                     >
-
-                        <Form.Group className="mb-3" controlId="formBasicEmail" >
-                            <div className="GrouInputLogin">
-                                <Form.Label style={{color: "#D95843"}}>Email </Form.Label>
-                                <Form.Control type="email" placeholder="Email" />
-                            </div>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <div className="GrouInputLogin"> 
-                                <Form.Label style={{color: "#D95843"}}>Senha </Form.Label>
-                                <Form.Control type="password" placeholder="Senha" />
-                            </div>
-                   
-                        </Form.Group>       
-                        <div className="BtnLoginDiv">
-                            <Button  type="submit" className="BtnLogin">
-                                Login
-                            </Button>
+                        <Col>
+                            <img
+                                className="FundoLogin"
+                                src={BannerLogin}
+                            />
+                        </Col>
+                        <Col className="EmailpwdSide">
+                        <div style={{marginTop: 20}}>
+                            <h1 style={{color: "#D95843"}}>Login</h1>
                         </div>
-                    </Form>
+
+                        <Form
+                            className="FormLoginWidht"
+                        >
+
+                            <Form.Group className="mb-3" controlId="formBasicEmail" >
+                                <div className="GrouInputLogin">
+                                    <Form.Label style={{color: "#D95843"}}>Email </Form.Label>
+                                    <Form.Control type="email" placeholder="Email" />
+                                </div>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <div className="GrouInputLogin"> 
+                                    <Form.Label style={{color: "#D95843"}}>Senha </Form.Label>
+                                    <Form.Control type="password" placeholder="Senha" />
+                                </div>
+                    
+                            </Form.Group>       
+                            <div className="BtnLoginDiv">
+                                <Button  type="submit" className="BtnLogin">
+                                    Login
+                                </Button>
+                            </div>
+                        </Form>
 
 
-                    <Col className="GroupPwdReg1">
-                                <p style={{color: "#D95843"}}>Esqueceu a Senha ?</p>
-                                <Link to="/about" className="LinkName"> Clique Aqui</Link>
-                            </Col>
-                            <Col className="GroupPwdReg1">
-                                <p style={{color: "#D95843"}}>Não tem um Login ?</p>
-                                <Link to="/about" className="LinkName">Cadastra-se</Link>
-                            </Col>
-                    </Col>
-                     
-                </Row>
+                        <Col className="GroupPwdReg1">
+                                    <p style={{color: "#D95843"}}>Esqueceu a Senha ?</p>
+                                    <Link to="/about" className="LinkName"> Clique Aqui</Link>
+                                </Col>
+                                <Col className="GroupPwdReg1">
+                                    <p style={{color: "#D95843"}}>Não tem um Login ?</p>
+                                    <Link to="/about" className="LinkName">Cadastra-se</Link>
+                                </Col>
+                        </Col>
+                    </Row>
                 </div>
             </div>
         </>
